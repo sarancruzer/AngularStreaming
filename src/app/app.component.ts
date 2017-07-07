266d0e4dddaac8b1ab00fd6d7297144a39bde44e
Jeroenouw <jeroenouw@hotmail.com>
2017-07-07 10:39:53 +0200

    Firebase initialize and setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    firebase.initializeApp({
+      // For other projects use different keys
+      apiKey: "AIzaSyBbLtapmA7Li32ccmgjDSThSJ-wKf-Wk4Y",
+      authDomain: "angularstreaming.firebaseapp.com",
+      databaseURL: "https://angularstreaming.firebaseio.com",
+      projectId: "angularstreaming",
+      storageBucket: "",
+      messagingSenderId: "420594823747"
+    });
+  }
 }
