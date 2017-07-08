6d405935ec4e9c77a4906e133bf796bd9e746817
Jeroenouw <jeroenouw@hotmail.com>
2017-07-08 09:40:08 +0200

    Artists get data

--- src/app/shared/services/artists-service/artists.service.ts
+++ src/app/shared/services/artists-service/artists.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/Rx';
 
 @Injectable()
 export class ArtistsService {
+  artistUrl: string;
 
-  constructor() { }
+  constructor(private http: Http,
+              private res: Response) { }
+    
+  getArtist(id:string){
+      this.artistUrl = 'https://api.spotify.com/v1/artists/' + id;
+      return this.http.get(this.artistUrl)
+          .map(res => res.json());
+  }   
+
+  getArtistTopTracks(id:string){
+      this.artistUrl = 'https://api.spotify.com/v1/artists/+' + id + '/top-tracks';
+      return this.http.get(this.artistUrl)
+          .map(res => res.json());
+  }
+
+  getSeveralArtists(ids:string){
+      this.artistUrl = 'https://api.spotify.com/v1/artists?ids=' + ids;
+      return this.http.get(this.artistUrl)
+          .map(res => res.json());
+  }
 
 }
